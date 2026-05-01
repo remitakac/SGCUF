46039063433400551a785b2429428199ef2f0344
remitakac <remitakac@gmail.com>
2026-05-01 17:22:19 +0200

    Update comments from Slovak to English

--- test_decode.py
+++ test_decode.py
@@ -1,8 +1,7 @@
 from sgcuf_format import decode_sgcuf
 
-# Dekóduje SGCUF súbor a uloží výsledný PNG
+# Decodes an SGCUF file and saves the resulting PNG
 img = decode_sgcuf("output/real_test.sgcuf")
 img.save("output/real_test_decoded.png")
 
-print("Hotovo: decoded → output/real_test_decoded.png")
-
+print("Done: decoded → output/real_test_decoded.png")
